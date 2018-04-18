2b7bdf4f80085d16cf7432e5f25d6eb6a90712be
mkphrvdds18 <29808555+mkphrvdds18@users.noreply.github.com>
2018-04-18 10:46:32 -0400

    Schedule a task - initial file
    Schedule a task at some interval , this file has many uses. It can be changed to adjust many needs.

---
+++ schedule_a_task.py
@@ -0,0 +1,37 @@
+
+# code tested on anaconda, basic package did not have schedule, pls install items
+# pip install schedule
+# to stop the job, pls press ctrl+c from keyboard
+
+import schedule
+import time
+
+from datetime import datetime
+
+time_now = datetime.now()
+
+def job():
+  print("I will do a print job for now ..and each interval..")
+  print("you can run anything - like checking some dynamic content")
+  print("Current time: ", time_now)
+
+# run the job 1st time then start after the interval
+
+job()
+schedule.every(1).minutes.do(job)
+
+'''
+# these are very good example 
+
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+schedule.every(5).to(10).days.do(job)
+schedule.every().monday.do(job)
+schedule.every().wednesday.at("13:15").do(job)
+
+'''
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
